d959b41317ecb4648e19053e129b2649d186beb3
Christian-Sav <chris.sav@outlook.com>
2022-04-25 15:14:42 +0000

    EC2 & VPC partially implimented

---
+++ Terraform/EC2/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {
+    description = "The ID of the VPC to associate resources"
+}
+
+variable "subnet_id" {
+    description = "The ID of the subney to access the VPC"
+}
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region                   = "eu-west-2"
+  shared_credentials_files = ["~/.aws/credentials"]
 
+module "VPC" {
+  source = "./VPC"
+}
+
+module "EC2" {
+  source = "./EC2"
+  vpc_id = module.Proj-VPC.vpc_id
+}
