ef4b1b48296e78da5eb0c0872bbe145861ffe33a
majidub2 <99325840+majidub2@users.noreply.github.com>
2022-04-26 09:48:18 +0100

    Update kubescripts.sh

--- jenkins/kubescripts.sh
+++ jenkins/kubescripts.sh
@@ -3,7 +3,7 @@ sudo docker-compose build
 sudo docker-compose push
 ### TO DO ###
 # Add docker-compose to path
-aws eks update-kubeconfig --name QA_cluster
+aws eks update-kubeconfig --name QA-cluster
 kubectl apply -f kubernetes/backend.yaml
 kubectl apply -f kubernetes/frontend.yaml
 kubectl apply -f kubernetes/nginx.yaml
