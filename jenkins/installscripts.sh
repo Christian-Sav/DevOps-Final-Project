2d4fd2ca7c9bb80db0cc56b440216961ccf5dbaa
Christian-Sav <99397237+Christian-Sav@users.noreply.github.com>
2022-04-27 13:00:59 +0100

    Update installscripts.sh

--- jenkins/installscripts.sh
+++ jenkins/installscripts.sh
@@ -29,7 +29,7 @@ sudo mv terraform /usr/local/bin
 ## Check if terraform working properly
 terraform --version
 
-aws configure set aws_access_key_id "$ACCESS_KEY"
-aws configure set aws_secret_access_key "$SECRET_KEY"
+aws configure set aws_access_key_id "$ACCESS_KEY_PSW"
+aws configure set aws_secret_access_key "$SECRET_KEY_PSW"
 aws configure set region "eu-west-2"
 aws configure set output "json"
