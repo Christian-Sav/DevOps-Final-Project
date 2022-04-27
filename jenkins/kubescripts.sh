a265f8aaab9b965c7eb4e6827be649a49dc77292
Christian-Sav <chris.sav@outlook.com>
2022-04-27 12:55:56 +0000

    updfated kubescripts

--- jenkins/kubescripts.sh
+++ jenkins/kubescripts.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo docker-compose build
-sudo docker-compose push
-
 aws eks update-kubeconfig --name QA-cluster
 kubectl apply -f kubernetes/backend.yaml
 kubectl apply -f kubernetes/frontend.yaml
