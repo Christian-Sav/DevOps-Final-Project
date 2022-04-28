6c9c128a6d14770df67569e9a28dff8d32e411ee
Christian-Sav <chris.sav@outlook.com>
2022-04-28 14:23:54 +0000

    changes

--- jenkins/installscripts.sh
+++ jenkins/installscripts.sh
@@ -18,3 +18,8 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/l
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+sudo apt install awscli -y
+aws configure set aws_access_key_id "$ACCESS_KEY_PSW"
+aws configure set aws_secret_access_key "$SECRET_KEY_PSW"
+aws configure set region "eu-west-2"
+aws configure set output "json"
